26c4a623a0f509bb595b9ac49bb458efd78e943f
vimbai1234 <166508171+vimbai1234@users.noreply.github.com>
2024-08-26 19:59:36 +0200

    Add files via upload

---
+++ BANK/src/Main.java
@@ -0,0 +1,11 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+
+        // Creating an instance of the Bank class
+        Bank myBankAccount = new Bank("VBC", 230634);
+        myBankAccount.display(); // Invoking the display method
+
+        }
+    }
